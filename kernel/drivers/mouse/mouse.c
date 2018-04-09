54a48022f100da353352d6a286383fcee64871cd
jack <jack.britton@maine.edu>
2018-04-09 11:48:31 -0400

    Finished parsing bits.

--- kernel/drivers/mouse/mouse.c
+++ kernel/drivers/mouse/mouse.c
@@ -14,21 +14,25 @@ int mouse_init(void) {
     uint32_t status;
 
     // Request the clock and data pins.
+
     status = gpio_request(gpio_clock, "mouse_clock");
     if (status < 0) return -1;
     status = gpio_request(gpio_clock, "mouse_data");
     if (status < 0) return -1;
 
     // Those are inputs.
+
     gpio_direction_input(gpio_clock);
     gpio_direction_input(gpio_data);
 
     // Get the IRQ number for the clock,
     // since that's what we'll be watching.
+
     irq = gpio_to_irq(gpio_clock);
     if (irq < 0) return -1;
 
     // Watch the falling edge and enable.
+
     gpio_set_falling(gpio_clock);
     irq_enable(irq);
 
@@ -40,15 +44,93 @@ int mouse_init(void) {
 void mouse_cleanup(void) {
 
     // Release the GPIO pins.
+
 	gpio_free(gpio_data);
 	gpio_free(gpio_clock);
 
 }
 
-int mouse_interrupt_handler(void) {
+static void mouse_update(char bytes[3]) {
 
     // TODO
 
+}
+
+int mouse_interrupt_handler(void) {
+
+    static int byte_counter = 0,
+               bit_counter  = 0,
+               parity       = 0;
+    static char bytes[3]    = { 0, 0, 0 };
+    int bit;
+
+    bit = gpio_get_value(gpio_data);
+
+    switch (bit_counter) {
+
+    case  0: // Start bit.
+
+        if (bit != 0) {
+            printk("Invalid start bit %x.\n", bit);
+            goto mouse_interrupt_handler_failure;
+        }
+
+        break;
+
+    case  9: // Parity bit.
+
+        parity += bit;
+        if (parity & 0x1 == 0) { // It's even (should be odd!).
+            printk("Invalid parity %x.\n", parity);
+            goto mouse_interrupt_handler_failure;
+        }
+        parity = 0;
+
+        break;
+
+    case 10: // Stop bit.
+
+        if (bit != 1) {
+            printk("Invalid stop bit %x.\n", bit);
+            goto mouse_interrupt_handler_failure;
+        }
+
+        byte_counter = (byte_counter + 1) % 3;
+        if (byte_counter == 0) {
+
+            // We just finished reading the third byte.
+
+            mouse_update(bytes);
+
+        }
+
+        break;
+    
+    default: // Data bits.
+
+        // Put the bit into the byte and add to parity.
+
+        bytes[byte_counter] |= bit << (bit_counter-1);
+        parity += bit;
+
+        break;
+
+    }
+
+    // Increment bit_counter.
+
+    bit_counter = (bit_counter + 1) % 11;
+
     return 0;
+
+mouse_interrupt_handler_failure:
+
+    bit_counter  = 0;
+    parity       = 0;
+    byte_counter = 0;
+    bytes[0]     = 0;
+    bytes[1]     = 0;
+    bytes[2]     = 0;
+    return -1;
 
 }
